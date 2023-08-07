8974c8539c31245e4dea30ebdc472c40944f272a
Thomas Brooks <tgbrooks@gmail.com>
2023-08-07 11:03:35 -0400

    Move DESeq2 to the original file

--- dependent_sim.R
+++ dependent_sim.R
@@ -15,37 +15,45 @@ get_random_structure <- function(data, rank, type="normal") {
     )
     # Trivial transform: just standardize the already normal data
     transformed_data <- (data - mean) / var
-    
   } else if (type == "poisson") {
     marginals <- list(
       lambda = apply(data, 1, mean)
     )
     transformed_data <- qnorm(ppois(data, marginals$lambda))
+  } else if (type == "DESeq2") {
+    fit <- fit_deseq(data)
+    marginals <- fit$marginals
+    transformed_data <- fit$transformed_data
   } else {
-    stop("'type' must be one of: 'normal', 'poisson'")
+    stop("'type' must be one of: 'normal', 'poisson', 'DESeq2'")
   }
-  
+
   # Cov structure from PCA of the (transformed to have normal marginals) data
-  pc <- prcomp(t(transformed_data), rank. = rank, center = FALSE, scale. = FALSE)
+  udv <- svd(transformed_data, nu=rank, nv=0)
+  udv$d <- udv$d / sqrt(dim(data)[2])
+
   # We compute variances without centering the data - since the transformation to normal
-  # already 'centers' it (same as center=FALSE in prcomp, otherwise we get negatives in the draw function)
-  variances <- apply(transformed_data^2, 1, sum)
-  
+  # already 'centers' it (same reason we don't center/scale before SVD)
+  variances <- apply(transformed_data^2, 1, mean)
+
   return(list(
     type = type,
     rank = rank,
     marginals = marginals,
-    cov = pc,
+    cov = udv,
     var = variances,
     n_features = dim(transformed_data)[1],
     transformed_data = transformed_data
   ))
 }
 
-  draw_from_multivariate_corr <- function(random_structure, n_samples) {
+draw_from_multivariate_corr <- function(random_structure, n_samples, size_factors=NULL) {
   # Samples from a multivariate distribution that has:
   # - each variable has the specified marginal distribution
   # - the same covariance in the top principal components observed in (normalized) data
+  # 'size_factors' is used only when random_structure$type is DESeq2, in which case it is a vector
+  # of length n_samples with the size factors (1 means average library size, 2 means twice the read depth, etc.)
+  # By default size_factors is set to all 1s, so all libraries have approximately the same size
   
   k <- random_structure$rank
   marginals <- random_structure$marginals
@@ -56,33 +64,83 @@ get_random_structure <- function(data, rank, type="normal") {
   # Draw from the multivariate normal distribution with the dependence structure of the pc
   # but done efficiently by transforming to a standard normal
   indep_draws <- matrix(rnorm(k*n_samples), c(k, n_samples))
-  sdev <- diag(head(random_structure$cov$sdev, k), nrow=k)
-  pc_draws <- pc$rotation %*% sdev %*% indep_draws
+  sdev <- diag(head(random_structure$cov$d, k), nrow=k)
+  pc_draws <- pc$u %*% sdev %*% indep_draws
   
   # Add in the missing variance to match the actual data
   # by drawing independent data with the appropriate variance
-  # Each variable has variance 1 since it has been transformed to standard normal
-  missing_var <- random_structure$var - (pc$rotation %*% sdev)^2
-  print(missing_var)
+  pc_var <- apply((pc$u %*% sdev)^2, 1, sum) # variance we already accounted for
+  missing_var <- pmax(random_structure$var - pc_var, 0) # variance left to include as purely independent
   indep_draws <- matrix(rnorm(n_features*n_samples, sd=rep(sqrt(missing_var), n_samples)), c(n_features, n_samples))
 
-  # Random draws with normal marginals
   transformed_draws <- pc_draws + indep_draws
 
-  # Correct the draws to have the correct marginals
+  # Adjust the draws to have the correct marginals
   if (type == "normal") {
     return(transformed_draws * sqrt(marginals$var) + marginals$mean)
   } else if (type == "poisson") {
     return(qpois(pnorm(transformed_draws), lambda = marginals$lambda))
+  } else if (type == "DESeq2") {
+    if (is.null(size_factors)) { size_factors <- rep(1, n_samples) }
+    s <- matrix(size_factors, nrow=n_features, ncol=n_samples, byrow=TRUE)
+    mu <- s * marginals$q
+    draws <- qnbinom(
+      pnorm(transformed_draws),
+      mu = mu,
+      size = 1/marginals$dispersion,
+    )
+    draws[is.na(draws)] <- 0 # NAs comes from the all-zero rows in the original data
+    return(draws)
   } else {
-    stop("'marginals$type' must be one of 'normal' or 'poisson'")
+    stop("'marginals$type' must be one of: 'normal', 'poisson', 'DESeq2'")
   }
 }
 
- ## Test
-library(MASS)
-norm_data <- t(mvrnorm(n=20, mu=c(0,0, 0), Sigma=matrix(c(1, 0.8, 0, 0.8, 1, 0, 0, 0, 1), c(3,3))))
-pois_data <- qpois(pnorm(norm_data), lambda = c(50, 1000, 2))
+fit_deseq <- function(data) {
+  # Use DESeq2 to fit the marginal distributions (negative binomial)
+  dummy <- as.matrix(rep(1, dim(data)[2]))
+  rownames(dummy) <- colnames(data)
+  colnames(dummy) <- "dummy"
 
-rs <- get_random_structure(pois_data, rank=1, type="poisson")
-draws <- draw_from_multivariate_corr(rs, 30)
+  dds <- DESeq2::DESeqDataSetFromMatrix(
+    as.matrix(data),
+    dummy,
+    ~ 1
+  )
+  dds <- DESeq2::DESeq(dds)
+
+  # Use the DESeq-fit model to transform the data to approximate normal
+  # DESeq has the following model:
+  # Each X_{ij} ~ NB(mu_ij, alpha_i)
+  #      mu_ij = s_j q_{ij}
+  #      log_2(q_{ij}) = x_j Beta_i
+  # We use the trivial linear model so x_j = 1 and Beta_i is just an Intercept term
+  # s_j is the sample-specific size factor to account for differences in read depths
+  # alpha_i is the gene's dispersion parameter
+  # perform the transformation via negative binomial -> probability -> normal distribution
+  q <- 2^mcols(dds)$Intercept
+  s <- dds$sizeFactor
+  mu <- matrix(s, nrow=nrow(data), ncol=ncol(data), byrow=TRUE) * matrix(q, nrow=nrow(data), ncol=ncol(data))
+
+  if (is.null(assays(dds)$replaceCounts)) {
+    counts <- data
+  } else {
+    # DESeq2 has removed outliers, so we use that data
+    counts <- assays(dds)$replaceCounts
+  }
+  transformed_data <- qnorm(pnbinom(
+    counts,
+    mu = mu,
+    size = 1/dispersions(dds),
+  ))
+
+  # DESeq gives NAs where it can't fit. We replace those with zeros
+  transformed_data[is.na(transformed_data)] <- 0
+  return(list(
+    marginals = list(
+      q = q,
+      dispersion = dispersions(dds)
+    ),
+    transformed_data = transformed_data
+  ))
+}

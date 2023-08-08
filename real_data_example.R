ad3373cde9776a360ca8d15ec5e194bc9bd9fb9f
Thomas Brooks <tgbrooks@gmail.com>
2023-08-08 17:25:50 -0400

    Expand real data example

--- real_data_example.R
+++ real_data_example.R
@@ -4,7 +4,7 @@ library(ggplot2)
 # Load the real data ---------------------------------
 liver <- read.delim("C:/Users/tgb/data/circadian_controls/results/Liver/num_reads_all_samples.txt")
 # we choose just 8 samples to use from this large dataset
-read_data <- as.matrix(liver[ ,3:10])
+read_data <- as.matrix(liver[ ,3:14])
 mode(read_data) <- "integer"
 rownames(read_data) <- liver[ ,1]
 
@@ -15,60 +15,57 @@ draws <- draw_from_multivariate_corr(rs, n_samples=100)
 rs_indep <- remove_dependence(rs)
 indep_draws <- draw_from_multivariate_corr(rs_indep, n_samples=100)
 
+cpm <- function(x) { # counts per million
+  return(t(t(x) / apply(x, 2, sum) * 1e6))
+}
+scaled_read_data <- cpm(read_data)
+scaled_draws <- cpm(draws)
+scaled_indep_draws <- cpm(indep_draws)
+
 # Plot marginal distributions --------------------------
-simulated_mean <- apply(draws, 1, mean)
-simulated_variance <- apply(draws, 1, var)
-real_mean <- apply(read_data, 1, mean)
-real_variance <- apply(read_data, 1, var)
+simulated_mean <- apply(scaled_draws, 1, mean)
+simulated_variance <- apply(scaled_draws, 1, var)
+real_mean <- apply(scaled_read_data, 1, mean)
+real_variance <- apply(scaled_read_data, 1, var)
 plot(log(real_mean+1), log(simulated_mean+1))
 plot(log(real_variance+1), log(simulated_variance+1))
 
 # Plot correlation -------------------------------------
-# Randomly sample pairs of genes to assess correlation
-N_pairs = 10000
-real_corr <- rep(NA, N_pairs)
-simulated_corr <- rep(NA, N_pairs)
-for (i in 1:N_pairs) {
-  while(TRUE) {
-    gene1 <- sample(nrow(read_data), 1)
-    if (var(read_data[gene1, ]) != 0
-          && var(draws[gene1, ]) != 0) {
-      break
-    }
-  }
-  while(TRUE) {
-    gene2 <- sample(nrow(read_data), 1)
-    if (var(read_data[gene2, ]) != 0
-        && var(draws[gene2, ]) != 0
-        && gene1 != gene2) {
-          break
-    }
-  }
-  real_corr[i] <- cor(read_data[gene1, ], read_data[gene2, ])
-  simulated_corr[i] <- cor(draws[gene1, ], draws[gene2, ])
-}
-plot(real_corr, simulated_corr)
+N_rows <- 1000
+high_expr_rows <- which(apply(read_data, 1, mean) > 100)
+sample_rows <- sample(high_expr_rows, N_rows)
+real_corr <- cor(t(scaled_read_data[sample_rows,]))
+sim_corr <- cor(t(scaled_draws[sample_rows,1:ncol(read_data)]))
+indep_sim_corr <- cor(t(scaled_indep_draws[sample_rows,1:ncol(read_data)]))
+probs <- (1:1000)/1000
+real_corr_q <- quantile(real_corr, probs = probs, na.rm=TRUE)
+sim_corr_q <- quantile(sim_corr, probs = probs, na.rm=TRUE)
+indep_sim_corr_q <- quantile(indep_sim_corr, probs = probs, na.rm=TRUE)
+ggplot(data = data.frame(
+    real_corr_q = real_corr_q,
+    sim_corr_q = sim_corr_q,
+    indep_sim_corr_q = indep_sim_corr_q
+  )) +
+  geom_point(aes(x = real_corr_q, y = sim_corr_q), color="red") +
+  geom_point(aes(x = real_corr_q, y = indep_sim_corr_q), color="blue") +
+  geom_abline(slope=1, intercept=0)
 
 # Plot the top principal components --------------------
 constant_rows <- apply(read_data, 1, function(x) all(x - mean(x) == 0))
-
 # using PCA
-#pca <- prcomp(t(read_data[!constant_rows,]), rank.=2, scale.=TRUE)
-#projected_read_data <- t(pca$rotation) %*% scale(read_data[!constant_rows, ])
-#projected_draws <- t(pca$rotation) %*% scale(draws[!constant_rows, ])
-#projected_indep_draws <- t(pca$rotation) %*% scale(indep_draws[!constant_rows, ])
-# using the existing projection from rs$cov
-projected_read_data <- t(rs$cov$u[!constant_rows,]) %*% scale(read_data[!constant_rows, ])
-projected_draws <- t(rs$cov$u[!constant_rows,]) %*% scale(draws[!constant_rows, ])
-projected_indep_draws <- t(rs$cov$u[!constant_rows,]) %*% scale(indep_draws[!constant_rows, ])
+# scaling each column to account for read depth differences
+pca <- prcomp(t(scale(read_data[!constant_rows,])), rank.=2, scale.=TRUE)
+projected_read_data <- predict(pca, t(scale(read_data[!constant_rows, ])))
+projected_draws <- predict(pca, t(scale(draws[!constant_rows, ])))
+projected_indep_draws <- predict(pca, t(scale(indep_draws[!constant_rows, ])))
 both_data <- data.frame(list(
-  x = c(projected_read_data[1,], projected_draws[1,], projected_indep_draws[1,]),
-  y = c(projected_read_data[2,], projected_draws[2,], projected_indep_draws[2,]),
+  PC1 = c(projected_read_data[,"PC1"], projected_draws[,"PC1"], projected_indep_draws[,"PC1"]),
+  PC2 = c(projected_read_data[,"PC2"], projected_draws[,"PC2"], projected_indep_draws[,"PC2"]),
   type = c(
-    rep("real", ncol(projected_read_data)),
-    rep("sim", ncol(projected_draws)),
-    rep("indep_sim", ncol(projected_indep_draws))
+    rep("real", nrow(projected_read_data)),
+    rep("sim", nrow(projected_draws)),
+    rep("indep_sim", nrow(projected_indep_draws))
   )
 ))
-ggplot(data = both_data, aes(x=x, y=y,color=type)) +
+ggplot(data = both_data, aes(x=PC1, y=PC2,color=type)) +
    geom_point()

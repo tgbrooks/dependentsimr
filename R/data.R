9dbf36e5e17cb4c6323aedf0b13968ea34c9aabc
Thomas Brooks <tgbrooks@gmail.com>
2023-08-15 15:35:53 -0400

    Use R data for the example data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Weger et al. 2018 RNA-seq of Male Mouse Liver
+#'
+#' Quantifications of bulk RNA-seq data of liver samples from 12 male mice.
+#' This includes only the conventionally raised mice from the experiment.
+#' Mice were collected around the clock at four hour intervals.
+#' See PMID: 30344015 for description of experimental and sequencing setup.
+#' See PMID: 37382061 for description of alignment and quantification using Salmon.
+#'
+#' @format ## `Weger18`
+#' A data frame with 40,614 rows and 12 columns.
+#' Each column is one mouse sample with it's corresponding GEO identifier.
+#' Each row is a ENSEMBL mouse gene/transcript with the quantified values for each sample.
+#'
+#' @source <https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE77221>
+"Weger18"
--- data/README.md
+++
@@ -1,7 +0,0 @@
-# Data Files
-## Weger18.txt.gz
-
-RNA-seq data obtained from GEO ([GSE77221](https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE77221)) of the male mouse liver conventionally raised samples.
-See: Weger BD, Gobet C, Yeung J, Martin E et al. The Mouse Microbiome Is Required for Sex-Specific Diurnal Rhythms of Gene Expression and Metabolism. Cell Metab 2019 Feb 5;29(2):362-382.e8. PMID: 30344015
-
-FASTQ files were quantified with Salmon with the GRCm38.75 genome, see [https://doi.org/10.1177/07487304231179600](https://doi.org/10.1177/07487304231179600).

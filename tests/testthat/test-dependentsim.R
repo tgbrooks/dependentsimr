6fac19a1c664917ce6d4d0b670cff4f4f928e5f0
Thomas Brooks <tgbrooks@gmail.com>
2023-08-16 11:43:52 -0400

    Expand testing

--- tests/testthat/test-dependentsim.R
+++ tests/testthat/test-dependentsim.R
@@ -2,7 +2,7 @@ set.seed(0)
 ## MULTIVARIATE NORMAL DATA ------------------------------------
 # Generate the example MV normal data
 library(MASS)
-Sigma = matrix(c(
+true_Sigma = matrix(c(
   1,      0.8,    0,  0,  0,  0,
   0.8,    1,      0,  0,  0,  0,
   0,      0,      1,  0,  0,  0,
@@ -10,7 +10,7 @@ Sigma = matrix(c(
   0,      0,      0,  0,  1,  0.3,
   0,      0,      0,  0,  0.3, 1
 ), nrow=6, ncol=6)
-norm_data <- t(mvrnorm(n=20, mu=c(0,0,0,0,0,0), Sigma=Sigma))
+norm_data <- t(mvrnorm(n=20, mu=c(0,0,0,0,0,0), Sigma=true_Sigma))
 
 # Simulate draws mimicking that data
 rs_normal <- get_random_structure(list(data=norm_data), rank=2, type="normal")
@@ -20,6 +20,19 @@ test_that("normal draws work", {
   expect_equal(dim(draws_normal$data), c(6,30))
 })
 
+# Compare the covariance matrix used to simulate with the expected properties
+# that it matches the desired diagonals and principal components
+pc_var <- apply((rs_normal$cov$u %*% diag(rs_normal$pc_factor_sizes))^2, 1, sum) # variance we already accounted for
+missing_var <- pmax(rs_normal$var - pc_var, 0) # variance left to include as purely independent
+Sigma <- rs_normal$cov$u %*% diag(rs_normal$pc_factor_sizes)^2 %*% t(rs_normal$cov$u) + diag(missing_var)
+Var <- rs_normal$transformed_data$data %*% t(rs_normal$transformed_data$data) / (ncol(rs_normal$transformed_data$data) - 1)
+test_that("pc size factors are accurate", {
+  expect_equal(diag(Sigma), rs_normal$var)
+  expect_equal((t(rs_normal$cov$u[,1]) %*% Sigma %*% rs_normal$cov$u[,1])[1,],
+               rs_normal$cov$d[1]^2)
+  expect_equal((t(rs_normal$cov$u[,2]) %*% Sigma %*% rs_normal$cov$u[,2])[1,],
+               rs_normal$cov$d[2]^2)
+})
 
 ## POISSON DATA ------------------------------------------------
 # Generate dependent Poisson data from the MV normal data
@@ -31,6 +44,7 @@ draws_poisson <- draw_from_multivariate_corr(rs_poisson, n_samples=30)
 
 test_that("poisson draws work", {
   expect_equal(dim(draws_poisson$data), c(6,30))
+  expect(all(draws_poisson$data >= 0), "draws_poisson$data must be all non-negative")
 })
 
 ## DESEQ2 DATA -------------------------------------------------
@@ -65,6 +79,7 @@ draws_deseq <- draw_from_multivariate_corr(rs_deseq, n_samples=30)
 
 test_that("DESeq2 draws work", {
   expect_equal(dim(draws_deseq$data), c(20,30))
+  expect(all(draws_deseq$data >= 0), "draws_deseq$data must be all non-negative")
 })
 
 
@@ -75,6 +90,7 @@ draws_emp <- draw_from_multivariate_corr(rs_emp, n_samples=30)
 
 test_that("empirical draws work", {
   expect_equal(dim(draws_emp$data), c(20,30))
+  expect_contains(read_counts, draws_emp$data)
 })
 
 ## Multi-omics fit ------------------------------------------------------
@@ -92,8 +108,6 @@ types <- list(
 
 rs_multi <- get_random_structure(datasets, rank=2, types=types)
 draws_multi <- draw_from_multivariate_corr(rs_multi, n_samples=10)
-
-
 
 test_that("multiple datasets works", {
   expect_equal(dim(draws_multi$norm), c(6,10))
